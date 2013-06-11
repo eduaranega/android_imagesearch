e7d7ea43005428c00a59ba145a56c08b426a0df9
Eduardo Henrique Dias Aranega <eduaranega@yahoo.com>
2013-06-10 20:14:32 -0700

    Android Image Search - initial version

---
+++ gen/com/android/eduardo/gridimagesearch/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.android.eduardo.gridimagesearch;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
